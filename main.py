0e960408322001dee36a0fec889e6139190f16cc
tarassss1 <143933998+tarassss1@users.noreply.github.com>
2024-10-05 19:12:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow # віджети
-from ui import Ui_MainWindow # імпорт інтерфейсу
-from PyQt6.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt6.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QFileDialog
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
 
-class Widget(Ui_MainWindow):
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtCore import QUrl
+
+class Widget(QMainWindow):
     def __init__(self):
-        super().__init__() 
+        super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.player = QMediaPlayer() # об'єкт для роботи з музикою
+        self.player = QMediaPlayer()
+        self.ui.select_button.clicked.connect(self.select_music)
+        self.ui.play_button.clicked.connect(self.play_music)
+        self.ui.stop_button.clicked.connect(self.stop_music)
 
     def select_music(self):
-        file, file1 = QFileDialog.getOpenFileName(self,
-                                                  "Select audio",
-                                                  "",
-                                                  "Audio Files(*.mp3 *.wav)")
+        file, n = QFileDialog.getOpenFileName(self, "Select audio", "", 
+                                              "Audio Files(*.mp3 *.wav)")
         if file:
             self.player.setMedia(QMediaContent(QUrl.fromLocalFile(file)))
-            self.player.setVolume(50)
+            self.player.setVolume(50) # встановлення гучності
+    
     def play_music(self):
         if self.player.state() == QMediaPlayer.PlayingState:
             self.player.pause()
         else:
-           self.player.play() 
+            self.player.play()
+
+    def stop_music(self):
+        self.player.stop()    
+    
+        
+
 
 app = QApplication([])
-window = Widget()
-window.show()
+ex = Widget()
+ex.show()
 app.exec_()
 
- 
 
-      
